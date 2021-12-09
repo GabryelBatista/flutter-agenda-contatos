4735ec6f97d809214fca9f209ce98013a85863b6
Gabryel Carreiro Batista <53455060+gabryelbatista@users.noreply.github.com>
2021-12-08 23:17:05 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_agenda_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_agenda_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_agenda_contatos/ui/home_page.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
